9526fd153c99791eb6644d2bce13d72344d8f138
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-08 07:00:37 +0000

    物件の登録機能を追加

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,11 +3,27 @@ class PropertiesController < ApplicationController
   end
 
   def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      redirect_to properties_path, notice: "物件を登録しました。"
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :rent, :address, :age, :note)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'properties/index'
-  get 'properties/new'
-  get 'properties/show'
-  get 'properties/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "properties#index"
+
+  resources :properties
 end
