f5f56f55a8f59c6f6b2a1af0429f5fc17ab98055
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-08 08:54:22 +0000

    物件情報の削除機能を追加

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,5 @@
 class PropertiesController < ApplicationController
-  before_action :set_property, only: [:show, :edit, :update]
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
 
   def index
     @properties = Property.all.order(created_at: :DESC)
@@ -30,6 +30,11 @@ class PropertiesController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_path, notice: "物件情報を削除しました。"
   end
 
   private
