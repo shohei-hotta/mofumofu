cb88c64f1f7091b575f6113b97936b2db72aa4ea
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-10 06:30:27 +0000

    最寄り駅登録時のバリデーションを追加

--- app/models/near_station.rb
+++ app/models/near_station.rb
@@ -1,3 +1,7 @@
 class NearStation < ApplicationRecord
   belongs_to :property
+
+  validates :route, presence: true
+  validates :name, presence: true
+  validates :minute, presence: true , numericality: true
 end
